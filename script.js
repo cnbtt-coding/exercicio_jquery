db300b4f1b5c8c9ceff219ae1403365c3c4aeed7
Caio Nascimento Battista <caio.caiobattista@outlook.com>
2023-08-22 20:16:31 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const novaTarefa = $('#nomeNovaTarefa').val();
+        const novoItem = $('<li></li>').text(novaTarefa);
+
+        $(novaTarefa).appendTo(novoItem);
+        $(novoItem).appendTo('ul'); 
+        $('#nomeNovaTarefa').val('');
+    })
+
+    $('#novo').click(function() {
+        $('#novo').hide();
+        $('.nova-tarefa').fadeIn();
+    })
+
+    $('#cancelar').click(function() {
+        $('#novo').fadeIn();
+        $('.nova-tarefa').hide();
+    })
+});
